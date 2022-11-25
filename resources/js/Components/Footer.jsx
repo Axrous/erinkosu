4d1b716a28f661b9ca7b04dde1d5812bbee63991
Axrous <argasatya16@gmail.com>
2022-11-25 17:11:34 +0700

    feat: add footer component

---
+++ resources/js/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <section className="">
+      <footer className="container mx-auto bg bg-[#334257] mt-10 h-16 inset-x-0 bottom-0 rounded-t-xl text-white text-xl tracking-widest flex items-center justify-center">
+        <span>Made with Love &#10084;</span>
+      </footer>
+    </section>
+  );
+}
