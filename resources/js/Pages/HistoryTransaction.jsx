c9ec7d0c7c3feb5524072d678cde46d74ffa0585
Axrous <argasatya16@gmail.com>
2022-12-07 18:17:30 +0700

    feat:integrate button detail to trasaction detail page

--- resources/js/Pages/HistoryTransaction.jsx
+++ resources/js/Pages/HistoryTransaction.jsx
@@ -1,7 +1,12 @@
 import CustomersLayout from "@/Layouts/CustomersLayout";
 import React from "react";
 
-export default function HistoryTransaction() {
+export default function HistoryTransaction({ transactions }) {
+  let timestamp = 1670392223895;
+  function date(timestamp) {
+    const dateTime = new Date(timestamp);
+    return dateTime;
+  }
   return (
     <CustomersLayout>
       <div className="container mx-auto">
@@ -9,70 +14,49 @@ export default function HistoryTransaction() {
           Transaction History
         </h1>
 
-        <div class="overflow-x-auto relative w-8/12 mx-auto rounded-xl">
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <div className="overflow-x-auto relative w-8/12 mx-auto rounded-xl">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                   Transaction Time
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                   Transaction ID
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                   Status
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                   Amount
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                   Action
                 </th>
               </tr>
             </thead>
             <tbody>
-              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <td class="py-4 px-6">Hari Senin</td>
-                <td class="py-4 px-6">TR-1000</td>
-                <td class="py-4 px-6">PENDING</td>
-                <td class="py-4 px-6">300000</td>
-                <td class="py-4 px-6">
-                  <button
-                    type="button"
-                    class="py-2 px-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Detail
-                  </button>
-                </td>
-              </tr>
-              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <td class="py-4 px-6">Hari Senin</td>
-                <td class="py-4 px-6">TR-2000</td>
-                <td class="py-4 px-6">SUCCESS</td>
-                <td class="py-4 px-6">300000</td>
-                <td class="py-4 px-6">
-                  <button
-                    type="button"
-                    class="py-2 px-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Detail
-                  </button>
-                </td>
-              </tr>
-              <tr class="bg-white dark:bg-gray-800">
-                <td class="py-4 px-6">Hari Senin</td>
-                <td class="py-4 px-6">TR-3000</td>
-                <td class="py-4 px-6">CANCEL</td>
-                <td class="py-4 px-6">300000</td>
-                <td class="py-4 px-6">
-                  <button
-                    type="button"
-                    class="py-2 px-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Detail
-                  </button>
-                </td>
-              </tr>
+              {transactions.map((transaction) => (
+                <tr
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                  key={transaction.id}
+                >
+                  <td className="py-4 px-6">
+                    {date(timestamp).toLocaleDateString()}
+                  </td>
+                  <td className="py-4 px-6">{transaction.id}</td>
+                  <td className="py-4 px-6 uppercase">{transaction.status}</td>
+                  <td className="py-4 px-6">300000</td>
+                  <td className="py-4 px-6">
+                    <a
+                      href={`/services/transaction-history/${transaction.id}`}
+                      className="py-2 px-6 text-s font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Detail
+                    </a>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
