e53c2155f92d1579b1bf601d2d6db36ae4319d40
Axrous <argasatya16@gmail.com>
2022-12-12 20:37:30 +0700

    change: change some features navbar right side

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -5,9 +5,11 @@ import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 export default function Navbar({ auth }) {
   const [navbar, setNavbar] = useState(false);
+  const [dropDown, setDropDown] = useState(false);
 
+  console.log(auth);
   return (
-    <nav className=" flex container mx-auto text-white py-2">
+    <nav className=" flex container mx-auto text-white py-2 items-center">
       <span className="w-6/12 md:w-3/12 text-2xl my-auto font-semibold tracking-widest">
         <Link href="/">ErinKosu</Link>
       </span>
@@ -40,19 +42,71 @@ export default function Navbar({ auth }) {
           </li>
         </ul>
       </div>
-      <div className="w-3/12 text-end hidden md:block ">
-        <button type="button" className="border rounded-full text-base">
-          {auth ? (
-            <Link href="/logout" className="py-2 px-10 inline-block">
-              Logout
-            </Link>
-          ) : (
+      <div className="w-3/12 text-end hidden md:block relative">
+        {auth ? (
+          <button className="" onClick={() => setDropDown(!dropDown)}>
+            {auth.first_name}
+            <svg
+              className="w-5 h-5 ml-1 inline-block"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        ) : (
+          <button type="button" className="border rounded-full text-base">
             <Link href="/login" className="py-2 px-10 inline-block">
               Login
             </Link>
-          )}
-        </button>
+          </button>
+        )}
+        {dropDown == true ? (
+          <div className="z-10 font-normal bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600 absolute right-0 top-10">
+            <ul
+              className="py-1 text-sm text-gray-700 dark:text-gray-400"
+              aria-labelledby="dropdownLargeButton"
+            >
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Dashboard
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Settings
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Earnings
+                </a>
+              </li>
+            </ul>
+            <div className="py-1">
+              <Link href="/logout" className="py-2 px-10 inline-block">
+                Logout
+              </Link>
+            </div>
+          </div>
+        ) : null}
       </div>
+
       <div className="w-6/12 md:hidden flex align-middle justify-end">
         <button
           type="button"
