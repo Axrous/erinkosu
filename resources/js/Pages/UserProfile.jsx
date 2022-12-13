e140a1258ca0edf9382efa47a4d1439e0e7084e3
Axrous <argasatya16@gmail.com>
2022-12-13 22:18:37 +0700

    feat: add profile section

--- resources/js/Pages/UserProfile.jsx
+++ resources/js/Pages/UserProfile.jsx
@@ -5,14 +5,24 @@ export default function UserProfile() {
   return (
     <CustomersLayout>
       <div className="container mx-auto border">
-        <div className="flex">
+        <div className="flex my-80">
           <div className="w-6/12 border">
-            <div className="w-6/12 mx-auto border ">
-              <div className="w-full">
-                <img src={deku} alt="" srcset="" className="object-fill" />
+            <div className="w-6/12 mx-auto border p-5 rounded-2xl shadow-2xl">
+              <img
+                src={deku}
+                alt="gambar"
+                className=" h-52 object-cover overflow-hidden w-full"
+              />
+              <div className="mt-4">
+                <ul>
+                  <li>Nama : Arga Satya</li>
+                  <li>Email : argasatya16@gmail.com</li>
+                  <li>No. Telp : 087811008278</li>
+                </ul>
               </div>
             </div>
           </div>
+          <div className="w-6/12 border"></div>
         </div>
       </div>
     </CustomersLayout>
