9b4d2377769cf5a97e45b67c9fd13438fab5db8c
Axrous <argasatya16@gmail.com>
2022-12-14 10:17:29 +0700

    feat:add design user section profile

--- resources/js/Pages/UserProfile.jsx
+++ resources/js/Pages/UserProfile.jsx
@@ -6,19 +6,41 @@ export default function UserProfile() {
     <CustomersLayout>
       <div className="container mx-auto border">
         <div className="flex my-80">
-          <div className="w-6/12 border">
-            <div className="w-6/12 mx-auto border p-5 rounded-2xl shadow-2xl">
+          <div className="w-6/12 ">
+            <div className="w-6/12 mx-auto p-5 rounded-2xl shadow-2xl">
               <img
                 src={deku}
                 alt="gambar"
                 className=" h-52 object-cover overflow-hidden w-full"
               />
               <div className="mt-4">
-                <ul>
-                  <li>Nama : Arga Satya</li>
-                  <li>Email : argasatya16@gmail.com</li>
-                  <li>No. Telp : 087811008278</li>
-                </ul>
+                <h3 className="mb-6 text-xl">PROFILE</h3>
+                <div className="">
+                  <div className="flex border-b-2 pb-1 mb-3">
+                    <span className="w-5/12 ">Nama Depan</span>
+                    <span className="w-7/12 text-start">Arga Satya</span>
+                  </div>
+                  <div className="flex border-b-2 pb-1 mb-3">
+                    <span className="w-5/12 ">Nama Belakang</span>
+                    <span className="w-7/12 text-start">Mulyono</span>
+                  </div>
+                  <div className="flex border-b-2 pb-1 mb-3">
+                    <span className="w-5/12 ">Email</span>
+                    <span className="w-7/12 text-start">
+                      argasatya16@gmail.com
+                    </span>
+                  </div>
+                  <div className="flex border-b-2 pb-1 mb-3">
+                    <span className="w-5/12 ">No. HP</span>
+                    <span className="w-7/12 text-start">087811008278</span>
+                  </div>
+                </div>
+                <button className="border px-4 py-2 rounded-lg bg-[#476072] text-white">
+                  Edit Profile
+                </button>
+                <button className="border px-4 py-2 rounded-lg bg-[#476072]  text-white ">
+                  Ubah Kata Sandi
+                </button>
               </div>
             </div>
           </div>
