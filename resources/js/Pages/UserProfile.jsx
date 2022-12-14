5e1f157e379cd4999664bd8f8c68ddc7eeca840d
Axrous <argasatya16@gmail.com>
2022-12-14 11:41:22 +0700

    feat: add user kost desc section

--- resources/js/Pages/UserProfile.jsx
+++ resources/js/Pages/UserProfile.jsx
@@ -1,38 +1,38 @@
 import CustomersLayout from "@/Layouts/CustomersLayout";
-import deku from "../../../public/photos/deku1.jpg";
+import profilePicture from "../../../public/photos/jojo.jpg";
 
 export default function UserProfile() {
   return (
     <CustomersLayout>
-      <div className="container mx-auto border">
-        <div className="flex my-80">
+      <div className="container mx-auto">
+        <div className="flex mt-20">
           <div className="w-6/12 ">
             <div className="w-6/12 mx-auto p-5 rounded-2xl shadow-2xl">
               <img
-                src={deku}
+                src={profilePicture}
                 alt="gambar"
-                className=" h-52 object-cover overflow-hidden w-full"
+                className=" h-52 object-cover overflow-hidden w-full object-top"
               />
               <div className="mt-4">
                 <h3 className="mb-6 text-xl">PROFILE</h3>
                 <div className="">
                   <div className="flex border-b-2 pb-1 mb-3">
                     <span className="w-5/12 ">Nama Depan</span>
-                    <span className="w-7/12 text-start">Arga Satya</span>
+                    <span className="w-7/12 text-start">Kujo</span>
                   </div>
                   <div className="flex border-b-2 pb-1 mb-3">
                     <span className="w-5/12 ">Nama Belakang</span>
-                    <span className="w-7/12 text-start">Mulyono</span>
+                    <span className="w-7/12 text-start">Jotaro</span>
                   </div>
                   <div className="flex border-b-2 pb-1 mb-3">
                     <span className="w-5/12 ">Email</span>
                     <span className="w-7/12 text-start">
-                      argasatya16@gmail.com
+                      starplatinum@gmail.com
                     </span>
                   </div>
                   <div className="flex border-b-2 pb-1 mb-3">
                     <span className="w-5/12 ">No. HP</span>
-                    <span className="w-7/12 text-start">087811008278</span>
+                    <span className="w-7/12 text-start">123123123123</span>
                   </div>
                 </div>
                 <button className="border px-4 py-2 rounded-lg bg-[#476072] text-white">
@@ -44,7 +44,28 @@ export default function UserProfile() {
               </div>
             </div>
           </div>
-          <div className="w-6/12 border"></div>
+          <div className="w-6/12">
+            <div className="w-10/12 border rounded-2xl shadow-xl pb-7">
+              <h3 className="text-2xl text-center my-4">Kamar Kost</h3>
+              <div className="flex border-b-2 pb-1 mb-3 w-8/12 mx-auto">
+                <span className="w-8/12">Kamar No. </span>
+                <span className="w-4/12 text-start">1</span>
+              </div>
+              <div className="flex border-b-2 pb-1 mb-3 w-8/12 mx-auto">
+                <span className="w-8/12">Tanggal Pemesanan </span>
+                <span className="w-4/12 text-start">12-12-2022</span>
+              </div>
+              <div className="flex border-b-2 pb-1 mb-3 w-8/12 mx-auto">
+                <span className="w-8/12">Tanggal Berakhir </span>
+                <span className="w-4/12 text-start">12-01-2023</span>
+              </div>
+              <div className="w-10/12 text-end">
+                <button className="border px-4 py-2 rounded-lg bg-[#476072] text-white">
+                  Perpanjangan
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </CustomersLayout>
