3d693a16de3fa5f4a862ef2257709191197b026e
Axrous <argasatya16@gmail.com>
2022-12-20 13:39:02 +0700

    style:change history transaction ui

--- resources/js/Pages/HistoryTransaction.jsx
+++ resources/js/Pages/HistoryTransaction.jsx
@@ -13,51 +13,33 @@ export default function HistoryTransaction({ transactions }) {
           Transaction History
         </h1>
 
-        <div className="overflow-x-auto relative w-8/12 mx-auto rounded-xl">
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" className="py-3 px-6">
-                  Transaction Time
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Transaction ID
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Status
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Amount
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Action
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {transactions.map((transaction) => (
-                <tr
-                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-                  key={transaction.id}
+        <div className="">
+          {transactions.map((transaction) => (
+            <div
+              className="border w-11/12 md:w-8/12 lg:w-6/12 mx-auto p-5 rounded-2xl bg-[#D9D9D9]"
+              key={transaction.id}
+            >
+              <div className="flex justify-between">
+                <span className="text-sm md:text-md uppercase">
+                  {transaction.id}
+                </span>
+                <span className="text-sm md:text-md">
+                  {date(transaction.booked_at).toLocaleDateString()}
+                </span>
+              </div>
+              <div className="flex items-center mt-20 justify-between">
+                <h2 className="text-sm md:text-md uppercase">
+                  Status: {transaction.status}
+                </h2>
+                <a
+                  href={`/services/transaction-history/${transaction.id}`}
+                  className="py-2 px-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  <td className="py-4 px-6">
-                    {date(transaction.booked_at).toLocaleDateString()}
-                  </td>
-                  <td className="py-4 px-6">{transaction.id}</td>
-                  <td className="py-4 px-6 uppercase">{transaction.status}</td>
-                  <td className="py-4 px-6">{transaction.amount}</td>
-                  <td className="py-4 px-6">
-                    <a
-                      href={`/services/transaction-history/${transaction.id}`}
-                      className="py-2 px-6 text-s font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    >
-                      Detail
-                    </a>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                  Detail
+                </a>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </CustomersLayout>
