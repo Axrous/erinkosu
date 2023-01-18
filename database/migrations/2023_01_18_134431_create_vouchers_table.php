a8fcec4f4cdc02583861062a8b00598179430726
Axrous <argasatya16@gmail.com>
2023-01-18 20:48:00 +0700

    feat:add voucer model and migration

---
+++ database/migrations/2023_01_18_134431_create_vouchers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('vouchers', function (Blueprint $table) {
+      $table->id();
+      $table->string("voucher_name");
+      $table->integer("discount_amount");
+      $table->integer("voucher_limit");
+      $table->integer("voucher_limit");
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('vouchers');
+  }
+};
