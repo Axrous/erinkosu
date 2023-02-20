787f06fff131235205aecb1fc7ca13c728ee3c73
Axrous <argasatya16@gmail.com>
2023-02-20 21:53:27 +0700

    change: change transaction history design

--- resources/js/Pages/HistoryTransaction.jsx
+++ resources/js/Pages/HistoryTransaction.jsx
@@ -14,33 +14,49 @@ export default function HistoryTransaction({ transactions }) {
           Transaction History
         </h1>
 
-        <div className="">
-          {transactions.map((transaction) => (
-            <div
-              className="border w-11/12 md:w-8/12 lg:w-6/12 mx-auto p-5 rounded-2xl bg-[#D9D9D9]"
-              key={transaction.id}
-            >
-              <div className="flex justify-between">
-                <span className="text-sm md:text-md uppercase">
-                  {transaction.id}
-                </span>
-                <span className="text-sm md:text-md">
-                  {date(transaction.booked_at).toLocaleDateString()}
-                </span>
-              </div>
-              <div className="flex items-center mt-20 justify-between">
-                <h2 className="text-sm md:text-md uppercase">
-                  Status: {transaction.status}
-                </h2>
-                <Link
-                  href={`/services/transaction-history/${transaction.id}`}
-                  className="py-2 px-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Detail
-                </Link>
-              </div>
-            </div>
-          ))}
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg w-7/12 mx-auto">
+          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" class="px-6 py-3">
+                  Order ID
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Date
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Status
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map((transaction) => (
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {transaction.id}
+                  </th>
+                  <td class="px-6 py-4">
+                    {date(transaction.booked_at).toLocaleDateString()}
+                  </td>
+                  <td class="px-6 py-4 uppercase">{transaction.status}</td>
+                  <td class="px-6 py-4 text-right">
+                    <Link
+                      href={`/services/transaction-history/${transaction.id}`}
+                      className="py-2 px-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Detail
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </CustomersLayout>
