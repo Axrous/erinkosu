33490c22310c5aa9d9f42f4a4bafcceef57d3a27
Axrous <argasatya16@gmail.com>
2023-02-21 21:02:09 +0700

    fix:fix padding in responsive

--- resources/js/Pages/HistoryTransaction.jsx
+++ resources/js/Pages/HistoryTransaction.jsx
@@ -88,7 +88,7 @@ export default function HistoryTransaction({ transactions }) {
             id="defaultModal"
             tabIndex="-1"
             aria-hidden="true"
-            className="fixed top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full bg-black bg-opacity-70 flex"
+            className="fixed top-0 left-0 right-0 z-50  w-full h-full pt-52 md:pt-0 px-4 md:px-0 md:p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full bg-black bg-opacity-70 flex"
           >
             <div className="relative w-full h-full max-w-2xl md:h-auto m-auto">
               {/* <!-- Modal content --> */}
