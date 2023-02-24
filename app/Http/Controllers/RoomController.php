ed42f0b12a5a1c299ed27d730f5fbbbec6852f53
Axrous <argasatya16@gmail.com>
2023-02-24 17:34:44 +0700

    feat: adding database transaction

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Enum\RoomStatusEnum;
 use App\Models\Room;
 use App\Models\RoomImage;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RoomController extends Controller
 {
@@ -13,25 +15,32 @@ class RoomController extends Controller
 
   public function addRoom(Request $request)
   {
-
     $validated = $request->validate([
       'no' => 'required',
-      'price' => 'required'
+      'price' => 'required',
+      'images' => 'required'
     ]);
-
-    $room = Room::create([
-      'no' => $request->no,
-      'is_booked' => false,
-      'price' => $request->price
-    ]);
-
-    foreach ($request->file('images') as $imageFile) {
-      $path = $imageFile->store('/images/resource', ['disk' => 'my_files']);
-      $image = RoomImage::create([
-        'url' => $path,
-        'room_no' => $room->no,
+    try {
+      DB::beginTransaction();
+      $room = Room::create([
+        'no' => $request->no,
+        'is_booked' => false,
+        'price' => $request->price
       ]);
+
+      foreach ($request->file('images') as $imageFile) {
+        $path = $imageFile->store('/images/resource', ['disk' => 'my_files']);
+        $image = RoomImage::create([
+          'url' => $path,
+          'room_no' => $room->no,
+        ]);
+      }
+    } catch (Exception $e) {
+      DB::rollBack();
+      return response()->json($e->getMessage(), 200);
     }
+    DB::commit();
+    return response()->json([$room], 200);
   }
 
   public function editRoom(Request $request, $room_no)
